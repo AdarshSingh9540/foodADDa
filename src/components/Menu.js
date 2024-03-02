e528fc2ec2ac256aef979f73e703c428b9cd2ca2
Adarsh <adarsh9540984202@gmail.com>
2024-03-02 20:25:22 +0530

    udate it

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -24,12 +24,12 @@ const Menu = () =>{
           <div className="bg-gray-500 h-64 flex items-center justify-center">
           <div className="left"><img className="w-64  h-48 rounded-md m-4" src={IMG_CDN_URL+res?.cards[0]?.card?.card?.info?.cloudinaryImageId} alt="" /></div>
          <div className=" text-center text-white">
-         <h2 className="text-4xl font-bold mx-12 " >{res?.cards[1]?.card?.card?.info?.name}</h2>
+         <h2 className="text-4xl font-bold mx-12 " >{res?.cards[0]?.card?.card?.info?.name}</h2>
           <h3 className="text-xl ml-4 mt-3 ">{res?.cards[0]?.card?.card?.info?.cuisines.join(" , ")}</h3>
-          <h3 className="text-xl"> {res?.cards[1]?.card?.card?.info?.areaName} , {res?.cards[1]?.card?.card?.info?.city}</h3>
-          <h3 className="text-xl">{res?.cards[1]?.card?.card?.info?.avgRating} ⭐️
+          <h3 className="text-xl"> {res?.cards[0]?.card?.card?.info?.areaName} , {res?.cards[0]?.card?.card?.info?.city}</h3>
+          <h3 className="text-xl">{res?.cards[0]?.card?.card?.info?.avgRating} ⭐️
           </h3>
-          <h3 className="text-xl">{res?.cards[1]?.card?.card?.info?.costForTwoMessage}</h3>
+          <h3 className="text-xl">{res?.cards[0]?.card?.card?.info?.costForTwoMessage}</h3>
          </div>
                </div>
 
