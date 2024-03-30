12d9f5f72b93ed457e630d5062e65346c8176678
Adarsh <adarsh9540984202@gmail.com>
2024-03-30 23:49:05 +0530

    css is added in header for small device

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,11 +19,11 @@ const cartItems = useSelector((store)=>store.cart.items);
       <div className=" container mx-auto flex justify-between h-20 items-center">
         <img className="m-2 w-16" src={Logo_CDN} alt="image" />
         <ul className="w-6/12 flex justify-between m-2 p-2 items-center font-semibold" >
-          <li> <Link className="" to="/">Home</Link> </li>
-          <li className=""><Link  to="/Contact">Contact Us</Link></li>
+          <li> <Link className=" " to="/">Home</Link> </li>
+          <li className="hidden sm:block"><Link  to="/Contact">Contact Us</Link></li>
           <li><Link  to="/Cart"> 🛒({cartItems?.length} )items</Link></li>
-          <li><Link  to="/About">About US</Link></li>
-          <li> <Link>{
+          <li className="hidden sm:block" ><Link  to="/About">About US</Link></li>
+          <li className="hidden sm:block"> <Link>{
       isLogIn ? <button className="bg-red-600 p-1 rounded-md mx-2" onClick={()=> setIsLogIn(false)} >Logout</button> : <button  className="bg-green-600 p-1 rounded-md mx-2" onClick={()=> setIsLogIn(true)}>Login</button> 
       }</Link></li>
         </ul>
